1cb45b5528e29de62d10033d60e3410067421079
helano <helano38@gmail.com>
2017-10-09 16:51:26 -0300

    Including tokens return
    -Returning Relational operators

--- Lexical analyzer.cpp
+++ Lexical analyzer.cpp
@@ -16,6 +16,71 @@ int ler_codigo(char string[]){
         int i=0;
         int length = strlen(string);
         printf("    token size: %d  ", length);
+        
+        
+        
+        
+        
+        
+        if (length <=2){
+        	if (length ==1){
+			
+	        	if (string[i]=='='){
+	        		return 4;
+	        	}
+	        	if(string[i]=='>'){
+					return 5;	
+				}
+				if(string[i]=='<'){
+					return 6;	
+				}
+				return -1;
+			}else {
+				 	if (string[i]=='='){
+	        		    if (string[i+1]=='='){
+	        		    	return 7;
+						}
+						return -1;
+	        		}else{
+	        			if (string[i]=='!'){
+							if(string[i+1]=='='){
+								return 8;
+							}else{
+								return -1;
+							}
+						}else{
+							if (string[i+1]=='='){
+								 if (string[i]=='>'){
+								 	return 9;
+								 }if (string[i]=='<'){
+								 	return 10;
+								 }else {
+								 	return -1;
+								 }
+								
+							}else {
+								return -1;
+							}
+							
+							
+						}
+	        		
+					}
+					
+				
+			}
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		else {
+		
        
               
 			if ( string[i]== 't'){
@@ -75,7 +140,7 @@ int ler_codigo(char string[]){
 					 } 
 					 
 			
-        
+        	}
         
         
 
@@ -93,17 +158,35 @@ void print_value (int i){
 		switch (i){
 		
 		case 1: 
-			printf ("Type Text \n");
+			printf ("Type : TEXT \n");
 			break;
 		case 2:
-		    printf ("Loop structure FOR\n")	;
+		    printf ("Loop structure: FOR\n")	;
 		    break;
 		case 3:
 		    printf("variable\n");
 		    break;
 		case 4: 
-			printf("operador relacional\n");
-			break;	
+			printf("Relational Operator : atribuicao\n");
+			break;
+		case 5:
+			printf("Relational Operator : MAJOR\n");
+			break;
+		case 6:
+			printf("Relational Operator : MINOR\n");
+			break;
+		case 7:
+			printf("oRelational Operator : IQUAL\n");
+			break;
+		case 8:
+			printf("Relational Operator : DIFFERENT\n");
+			break;
+		case 9:
+			printf("Relational Operator : MAJOR OR IQUAL\n");
+			break;
+		case 10:
+			printf("Relational Operator : MINOR OR IQUAL\n");
+			break;							
 		default: 
 			printf("can not indentify\n");
 			break;
